960674c1c92d8ea2cc5e26fa3ba22c2e336e0139
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-06-13 14:30:08 +0000

    fix: fixes in calculation

--- quality_addon/quality_addon/doctype/inline_stitching/inline_stitching.js
+++ quality_addon/quality_addon/doctype/inline_stitching/inline_stitching.js
@@ -1,184 +1,124 @@
-// Copyright (c) 2024, mohtashim and contributors
-// For license information, please see license.txt
-
 frappe.ui.form.on('Inline Stitching CT', {
-	double_stitch(frm, cdt, cdn) {
-		set_tax(frm, cdt, cdn);
+	inline_stitching_ct_add: function (frm, cdt, cdn) {
+		calculate_totals(frm);
 	},
-	open_seam(frm, cdt, cdn) {
-		set_tax(frm, cdt, cdn);
+	inline_stitching_ct_remove: function (frm, cdt, cdn) {
+		calculate_totals(frm);
 	},
-	loose_stitch(frm, cdt, cdn) {
-		set_tax(frm, cdt, cdn);
-	},
-	oil_stain(frm, cdt, cdn) {
-		set_tax(frm, cdt, cdn);
-	},
-	overlap_feb(frm, cdt, cdn) {
-		set_tax(frm, cdt, cdn);
-	},
-	puckering(frm, cdt, cdn) {
-		set_tax(frm, cdt, cdn);
-	},
-	raw_edge(frm, cdt, cdn) {
-		set_tax(frm, cdt, cdn);
-	},
-	spi(frm, cdt, cdn) {
-		set_tax(frm, cdt, cdn);
-	},
-	uneven_stitch(frm, cdt, cdn) {
-		set_tax(frm, cdt, cdn);
-	},
-	bad_stitch(frm, cdt, cdn) {
-		set_tax(frm, cdt, cdn);
-	},
-	open_safty(frm, cdt, cdn) {
-		set_tax(frm, cdt, cdn);
-	},
-	skip_stitch(frm, cdt, cdn) {
-		set_tax(frm, cdt, cdn);
-	},
-	boot_mark(frm, cdt, cdn) {
-		set_tax(frm, cdt, cdn);
-	},
-	damage(frm, cdt, cdn) {
-		set_tax(frm, cdt, cdn);
-	},
-	wrong_direction(frm, cdt, cdn) {
-		set_tax(frm, cdt, cdn);
-	},
-	number_label_missing(frm, cdt, cdn) {
-		set_tax(frm, cdt, cdn);
-	},
-	defects_qty(frm, cdt, cdn) {
-		set_tax(frm, cdt, cdn);
-	},
-});
-
-function set_tax(frm, cdt, cdn) {
-	var d = locals[cdt][cdn];
-	frappe.model.set_value(d.doctype, d.name, 'total_defect_pcs', d.double_stitch + d.defects_qty + d.open_seam + d.loose_stitch + d.oil_stain + d.overlap_feb + d.puckering + d.raw_edge + d.spi + d.uneven_stitch + d.bad_stitch + d.open_safty + d.skip_stitch + d.boot_mark + d.damage + d.wrong_direction + d.number_label_missing);
-}
-
-
-frappe.ui.form.on('Inline Stitching CT', {
-	total_defect_pcs: function (frm, cdt, cdn) {
-		var d = locals[cdt][cdn];
-		var total_defects = 0;
-		frm.doc.inline_stitching_ct.forEach(function (d) {
-			total_defects += d.total_defect_pcs;
-		});
-
-		frm.set_value('total_defects', total_defects);
-		refresh_field('total_defects');
-
-	}
-});
-
-frappe.ui.form.on('Inline Stitching CT', {
 	no_of_pcs: function (frm, cdt, cdn) {
-		let row = locals[cdt][cdn];
-		let double_stitch = 0;
-		let open_seam = 0;
-		let loose_stitch = 0;
-		let oil_stain = 0;
-		let overlap_feb = 0;
-		let puckering = 0;
-		let raw_edge = 0;
-		let spi = 0;
-		let defects_qty = 0;
-		let uneven_stitch = 0;
-		let bad_stitch = 0;
-		let open_safty = 0;
-		let skip_stitch = 0;
-		let boot_mark = 0;
-		let damage = 0;
-		let wrong_direction = 0;
-		let number_label_missing = 0;
-		let no_of_pcs = 0;
-		for (let i in frm.doc.inline_stitching_ct) {
-			double_stitch += frm.doc.inline_stitching_ct[i].double_stitch;
-			open_seam += frm.doc.inline_stitching_ct[i].open_seam;
-			loose_stitch += frm.doc.inline_stitching_ct[i].loose_stitch;
-			oil_stain += frm.doc.inline_stitching_ct[i].oil_stain;
-			overlap_feb += frm.doc.inline_stitching_ct[i].overlap_feb;
-			puckering += frm.doc.inline_stitching_ct[i].puckering;
-			raw_edge += frm.doc.inline_stitching_ct[i].raw_edge;
-			spi += frm.doc.inline_stitching_ct[i].spi;
-			defects_qty += frm.doc.inline_stitching_ct[i].defects_qty;
-			uneven_stitch += frm.doc.inline_stitching_ct[i].uneven_stitch;
-			bad_stitch += frm.doc.inline_stitching_ct[i].bad_stitch;
-			open_safty += frm.doc.inline_stitching_ct[i].open_safty;
-			skip_stitch += frm.doc.inline_stitching_ct[i].skip_stitch;
-			boot_mark += frm.doc.inline_stitching_ct[i].boot_mark;
-			damage += frm.doc.inline_stitching_ct[i].damage;
-			wrong_direction += frm.doc.inline_stitching_ct[i].wrong_direction;
-			number_label_missing += frm.doc.inline_stitching_ct[i].number_label_missing;
-			no_of_pcs += frm.doc.inline_stitching_ct[i].no_of_pcs;
-		}
-		frm.set_value('double_stitch_total', double_stitch);
-		frm.set_value('open_seam_total', open_seam);
-		frm.set_value('loose_stitch_total', loose_stitch);
-		frm.set_value('oil_stain_total', oil_stain);
-		frm.set_value('overlap_feb_total', overlap_feb);
-		frm.set_value('puckering_total', puckering);
-		frm.set_value('raw_edge_total', raw_edge);
-		frm.set_value('spi_total', spi);
-		frm.set_value('other_defect_qty', defects_qty);
-		frm.set_value('uneven_stitch_total', uneven_stitch);
-		frm.set_value('bad_stitch_total', bad_stitch);
-		frm.set_value('open_safty_total', open_safty);
-		frm.set_value('skip_stitch_total', skip_stitch);
-		frm.set_value('boot_mark_total', boot_mark);
-		frm.set_value('damage_total', damage);
-		frm.set_value('wrong_direction_total', wrong_direction);
-		frm.set_value('number_label_missing_total', number_label_missing);
-		frm.set_value('total_number_pcs', no_of_pcs);
-
-		frm.refresh();
+		calculate_totals(frm);
 	},
-	inline_stitching_ct: function (frm, cdt, cdn) {
-		frm.script_manager.trigger('double_stitch', cdt, cdn);
-		frm.script_manager.trigger('open_seam', cdt, cdn);
-		frm.script_manager.trigger('loose_stitch_total', cdt, cdn);
-		frm.script_manager.trigger('oil_stain_total', cdt, cdn);
-		frm.script_manager.trigger('overlap_feb_total', cdt, cdn);
-		frm.script_manager.trigger('puckering_total', cdt, cdn);
-		frm.script_manager.trigger('raw_edge_total', cdt, cdn);
-		frm.script_manager.trigger('spi_total', cdt, cdn);
-		frm.script_manager.trigger('other_defect_qty', cdt, cdn);
-		frm.script_manager.trigger('uneven_stitch', cdt, cdn);
-		frm.script_manager.trigger('bad_stitch', cdt, cdn);
-		frm.script_manager.trigger('open_safty', cdt, cdn);
-		frm.script_manager.trigger('skip_stitch', cdt, cdn);
-		frm.script_manager.trigger('boot_mark', cdt, cdn);
-		frm.script_manager.trigger('damage', cdt, cdn);
-		frm.script_manager.trigger('wrong_direction', cdt, cdn);
-		frm.script_manager.trigger('number_label_missing', cdt, cdn);
-		frm.script_manager.trigger('no_of_pcs', cdt, cdn)
-	},
-	inline_stitching_ct_remove(frm, cdt, cdn) {
-		frm.script_manager.trigger('double_stitch', cdt, cdn);
-		frm.script_manager.trigger('open_seam', cdt, cdn);
-		frm.script_manager.trigger('loose_stitch_total', cdt, cdn);
-		frm.script_manager.trigger('oil_stain_total', cdt, cdn);
-		frm.script_manager.trigger('overlap_feb_total', cdt, cdn);
-		frm.script_manager.trigger('puckering_total', cdt, cdn);
-		frm.script_manager.trigger('raw_edge_total', cdt, cdn);
-		frm.script_manager.trigger('spi_total', cdt, cdn);
-		frm.script_manager.trigger('other_defect_qty', cdt, cdn);
-		frm.script_manager.trigger('uneven_stitch', cdt, cdn);
-		frm.script_manager.trigger('bad_stitch', cdt, cdn);
-		frm.script_manager.trigger('open_safty', cdt, cdn);
-		frm.script_manager.trigger('skip_stitch', cdt, cdn);
-		frm.script_manager.trigger('boot_mark', cdt, cdn);
-		frm.script_manager.trigger('damage', cdt, cdn);
-		frm.script_manager.trigger('wrong_direction', cdt, cdn);
-		frm.script_manager.trigger('number_label_missing', cdt, cdn);
-		frm.script_manager.trigger('no_of_pcs', cdt, cdn)
+	before_save: function (frm, cdt, cdn) {
+		calculate_totals(frm);
+	}, double_stitch: function (frm, cdt, cdn) {
+		calculate_totals(frm);
+	}, open_seam: function (frm, cdt, cdn) {
+		calculate_totals(frm);
+	}, loose_stitch: function (frm, cdt, cdn) {
+		calculate_totals(frm);
+	}, oil_stain: function (frm, cdt, cdn) {
+		calculate_totals(frm);
+	}, overlap_feb: function (frm, cdt, cdn) {
+		calculate_totals(frm);
+	}, puckering: function (frm, cdt, cdn) {
+		calculate_totals(frm);
+	}, raw_edge: function (frm, cdt, cdn) {
+		calculate_totals(frm);
+	}, spi: function (frm, cdt, cdn) {
+		calculate_totals(frm);
+	}, defects_qty: function (frm, cdt, cdn) {
+		calculate_totals(frm);
+	}, uneven_stitch: function (frm, cdt, cdn) {
+		calculate_totals(frm);
+	}, bad_stitch: function (frm, cdt, cdn) {
+		calculate_totals(frm);
+	}, open_safty: function (frm, cdt, cdn) {
+		calculate_totals(frm);
+	}, skip_stitch: function (frm, cdt, cdn) {
+		calculate_totals(frm);
+	}, boot_mark: function (frm, cdt, cdn) {
+		calculate_totals(frm);
+	}, damage: function (frm, cdt, cdn) {
+		calculate_totals(frm);
+	}, wrong_direction: function (frm, cdt, cdn) {
+		calculate_totals(frm);
+	}, number_label_missing: function (frm, cdt, cdn) {
+		calculate_totals(frm);
+	}, no_of_pcs: function (frm, cdt, cdn) {
+		calculate_totals(frm);
+	}, total_defects: function (frm, cdt, cdn) {
+		calculate_totals(frm);
 	}
-})
 
+});
+
+function calculate_totals(frm) {
+	let totals = {
+		double_stitch: 0,
+		open_seam: 0,
+		loose_stitch: 0,
+		oil_stain: 0,
+		overlap_feb: 0,
+		puckering: 0,
+		raw_edge: 0,
+		spi: 0,
+		defects_qty: 0,
+		uneven_stitch: 0,
+		bad_stitch: 0,
+		open_safty: 0,
+		skip_stitch: 0,
+		boot_mark: 0,
+		damage: 0,
+		wrong_direction: 0,
+		number_label_missing: 0,
+		no_of_pcs: 0,
+		total_defects: 0
+	};
+
+	frm.doc.inline_stitching_ct.forEach(function (row) {
+		totals.double_stitch += row.double_stitch || 0;
+		totals.open_seam += row.open_seam || 0;
+		totals.loose_stitch += row.loose_stitch || 0;
+		totals.oil_stain += row.oil_stain || 0;
+		totals.overlap_feb += row.overlap_feb || 0;
+		totals.puckering += row.puckering || 0;
+		totals.raw_edge += row.raw_edge || 0;
+		totals.spi += row.spi || 0;
+		totals.defects_qty += row.defects_qty || 0;
+		totals.uneven_stitch += row.uneven_stitch || 0;
+		totals.bad_stitch += row.bad_stitch || 0;
+		totals.open_safty += row.open_safty || 0;
+		totals.skip_stitch += row.skip_stitch || 0;
+		totals.boot_mark += row.boot_mark || 0;
+		totals.damage += row.damage || 0;
+		totals.wrong_direction += row.wrong_direction || 0;
+		totals.number_label_missing += row.number_label_missing || 0;
+		totals.no_of_pcs += row.no_of_pcs || 0;
+		totals.total_defects += row.total_defect_pcs || 0;
+	});
+
+	frm.set_value('double_stitch_total', totals.double_stitch);
+	frm.set_value('open_seam_total', totals.open_seam);
+	frm.set_value('loose_stitch_total', totals.loose_stitch);
+	frm.set_value('oil_stain_total', totals.oil_stain);
+	frm.set_value('overlap_feb_total', totals.overlap_feb);
+	frm.set_value('puckering_total', totals.puckering);
+	frm.set_value('raw_edge_total', totals.raw_edge);
+	frm.set_value('spi_total', totals.spi);
+	frm.set_value('other_defect_qty', totals.defects_qty);
+	frm.set_value('uneven_stitch_total', totals.uneven_stitch);
+	frm.set_value('bad_stitch_total', totals.bad_stitch);
+	frm.set_value('open_safty_total', totals.open_safty);
+	frm.set_value('skip_stitch_total', totals.skip_stitch);
+	frm.set_value('boot_mark_total', totals.boot_mark);
+	frm.set_value('damage_total', totals.damage);
+	frm.set_value('wrong_direction_total', totals.wrong_direction);
+	frm.set_value('number_label_missing_total', totals.number_label_missing);
+	frm.set_value('total_number_pcs', totals.no_of_pcs);
+	frm.set_value('total_defects', totals.total_defects);
+
+	frm.refresh_fields();
+}
 frappe.ui.form.on('Inline Stitching', {
 	refresh(frm) {
 		frm.set_value('ds_percent', (frm.doc.double_stitch_total * 100) / frm.doc.total_number_pcs);
@@ -198,6 +138,8 @@ frappe.ui.form.on('Inline Stitching', {
 		frm.set_value('s_percent', (frm.doc.spi_total * 100) / frm.doc.total_number_pcs)
 		frm.set_value('ss_percent', (frm.doc.skip_stitch_total * 100) / frm.doc.total_number_pcs)
 		frm.set_value('nlm_percent', (frm.doc.number_label_missing_total * 100) / frm.doc.total_number_pcs)
+
+
 	},
 	before_Save(frm) {
 		frm.set_value('defect_percentage', (frm.doc.total_defects * 100) / frm.doc.total_number_pcs);
@@ -211,3 +153,18 @@ frappe.ui.form.on('Inline Stitching', {
 
 }
 );
+
+frappe.ui.form.on('Inline Stitching CT', {
+	no_of_pcs: function (frm, cdt, cdn) {
+		var d = locals[cdt][cdn];
+		var total_defects = 0;
+		frm.doc.inline_stitching_ct.forEach(function (d) {
+			total_defects += d.total_defect_pcs;
+
+		});
+
+		frm.set_value('total_defects', total_defects);
+		refresh_field('total_defects');
+
+	}
+});
