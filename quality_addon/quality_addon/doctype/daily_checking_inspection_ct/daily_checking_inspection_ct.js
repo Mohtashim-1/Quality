40ed0b112b1498e86034e35b5ef0aa8facefddb8
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2024-06-15 07:24:36 +0000

    bug: fixing

--- quality_addon/quality_addon/doctype/daily_checking_inspection_ct/daily_checking_inspection_ct.js
+++ quality_addon/quality_addon/doctype/daily_checking_inspection_ct/daily_checking_inspection_ct.js
@@ -4380,7 +4380,7 @@ frappe.ui.form.on('Daily Random Inspection', {
 
 
 	refresh(frm) {
-		frm.set_value('percent', (frm.doc.total_defects * 100) / frm.doc.total_sample_qty)
+		frm.set_value('total_percent', (frm.doc.total_defects * 100) / frm.doc.total_sample_qty)
 
 	}
 
